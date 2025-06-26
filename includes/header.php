a3436365bf2d89f8e70169e2b9338e61f37e02df
gustavo.domingues <gudomingues2009@gmail.com>
2025-06-26 15:45:45 -0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Catálogo de Filmes</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Catálogo de Filmes</h1>
+        <nav>
+            <a href="index.php">Início</a> |
+            <a href="adicionar.php">Adicionar Filme</a>
+        </nav>
+    </header>
+<main>
